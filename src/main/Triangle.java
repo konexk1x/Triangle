634a4587fb9d835277d3dc260f4c1ad74b6c95a7
konek1x <konexk1x@gmail.com>
2022-11-01 20:54:58 +0300

    Initial commit

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -1,12 +1,20 @@
 package main;
 
+import java.util.Scanner;
+
 public class Triangle {
     private byte A, B, C;
+    private Scanner scanner;
 
-    public Triangle(byte a, byte b, byte c) {
-        A = a;
-        B = b;
-        C = c;
+    public Triangle() {
+        scanner = new Scanner(System.in);
+        System.out.println("Введите стороны треугольника: ");
+        System.out.println("Сторона A: ");
+        this.A = scanner.nextByte();
+        System.out.println("Сторона B: ");
+        this.B = scanner.nextByte();
+        System.out.println("Сторона C: ");
+        this.C = scanner.nextByte();
     }
 
     public boolean notNullLength() {
@@ -21,5 +29,12 @@ public class Triangle {
             return true;
         } else
             return false;
+    }
+
+    public boolean outOfRange() {
+        if (A > Byte.MAX_VALUE || B > Byte.MAX_VALUE || C > Byte.MAX_VALUE) {
+            return false;
+        } else
+            return true;
     }
 }
