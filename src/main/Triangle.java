3dc8a16084b9d296b690666d21c9a391e50e5176
konek1x <konexk1x@gmail.com>
2022-11-01 20:09:33 +0300

    Initial commit

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -1,4 +1,25 @@
 package main;
 
 public class Triangle {
+    private int A, B, C;
+
+    public Triangle(int a, int b, int c) {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public boolean notNullLength() {
+        if (A == 0 || B == 0 || C == 0) {
+            return false;
+        } else
+            return true;
+    }
+
+    public boolean isTriangle() {
+        if (A + B > C && B + C > A && A + C > B) {
+            return true;
+        } else
+            return false;
+    }
 }
